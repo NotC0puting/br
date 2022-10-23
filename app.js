d71218a8de881d8ac391731fdb2f1ae613f592ff
NotC0puting <92543745+NotC0puting@users.noreply.github.com>
2022-10-23 18:37:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -158,3 +158,10 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 703px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes */
+
+const toggleButton = document.getElementsByClassName('dropdown-button')[0]
+const navbarLinks = document.getElementsByClassName('menuItem-B')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
